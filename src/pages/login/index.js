768c6023e49fd41ee6712358d4c004285a0f056c
liutaochange <liutaochange@163.com>
2019-04-16 18:43:18 +0800

    import redux

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,55 +1,23 @@
 import React from "react";
 import Logo from "@/components/logo";
 import { List, InputItem, WingBlank, WhiteSpace, Button } from "antd-mobile";
-import { Redirect, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: "",
-      pwd: ""
-    };
-    this.register = this.register.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-  }
-  register() {
-    this.props.history.push("/register.html");
-  }
-  handleChange(key, val) {
-    this.setState({
-      [key]: val
-    });
-  }
-  handleLogin() {
-    this.props.login(this.state);
-  }
   render() {
     return (
       <div>
-        {this.props.redirectTo ? <Redirect to={this.props.redirectTo} /> : null}
         <Logo />
         <WingBlank>
           <List>
-            {this.props.msg ? (
-              <p className="error-msg">{this.props.msg}</p>
-            ) : null}
-            <InputItem onChange={v => this.handleChange("user", v)}>
-              用户
-            </InputItem>
+            <InputItem>用户</InputItem>
             <WhiteSpace />
-            <InputItem onChange={v => this.handleChange("pwd", v)}>
-              密码
-            </InputItem>
+            <InputItem>密码</InputItem>
           </List>
           <WhiteSpace />
-          <Button onClick={this.handleLogin} type="primary">
-            登录
-          </Button>
+          <Button>登录</Button>
           <WhiteSpace />
-          <Button onClick={this.register} type="primary">
-            注册
-          </Button>
+          <Button>注册</Button>
         </WingBlank>
       </div>
     );
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Loading from '@/components/loading/index.js';
-const App = lazy(() => import('@/App.js'));
-const Login = lazy(() => import('@/pages/login'));
+import React, { lazy, Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Loading from "@/components/loading/index.js";
+const App = lazy(() => import("@/App.js"));
+const Login = lazy(() => import("@/pages/login"));
 const Routers = () => (
   <Router>
-    <Suspense fallback={<Loading />}>
+    <Suspense
+      fallback={() => {
+        Loading();
+      }}
+    >
       <Switch>
-        <Route exact path="/index.html" component={props => <App {...props} />} />
-        <Route exact path="/login.html" component={props => <Login {...props} />} />
+        <Redirect exact path="/" to="/index.html" />
+        <Route
+          exact
+          path="/index.html"
+          component={props => <App {...props} />}
+        />
+        <Route
+          exact
+          path="/login.html"
+          component={props => <Login {...props} />}
+        />
       </Switch>
     </Suspense>
   </Router>
