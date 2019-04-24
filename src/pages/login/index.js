0ad4bd32a3cf76d2bc7ee56a72b9f3b66bc74e4b
liutaochange <liutaochange@163.com>
2019-04-24 17:54:14 +0800

    add hooks

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "@/components/logo";
 import { List, InputItem, WingBlank, WhiteSpace, Button } from "antd-mobile";
 import { withRouter } from "react-router-dom";
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div>
-        <Logo />
-        <WingBlank>
-          <List>
-            <InputItem>用户</InputItem>
-            <WhiteSpace />
-            <InputItem>密码</InputItem>
-          </List>
-          <WhiteSpace />
-          <Button>登录</Button>
-          <WhiteSpace />
-          <Button>注册</Button>
-        </WingBlank>
-      </div>
-    );
+const Login = props => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [msg, setMsg] = useState('');
+  const login = () => {
+    useEffect(() => {
+    
+    })
   }
-}
+  return (
+    <div>
+      <Logo />
+      <WingBlank>
+        <List>
+          {msg?<p className='error-msg'>{msg}</p>:null}
+          <WhiteSpace />
+          <InputItem value={user} placeholder="请输入用户名" onChange={(val) => {
+            console.log(val)
+          }}>用户</InputItem>
+          <WhiteSpace />
+          <InputItem value={password} placeholder="请输入密码" onChange={(val) => {
+            console.log(val)
+          }}>密码</InputItem>
+        </List>
+        <WhiteSpace />
+        <Button onClick={login}>登录</Button>
+        <WhiteSpace />
+        <Button
+          onClick={() => {
+            props.history.push("/register.html");
+          }}
+        >
+          注册
+        </Button>
+      </WingBlank>
+    </div>
+  );
+};
 
 export default withRouter(Login);
